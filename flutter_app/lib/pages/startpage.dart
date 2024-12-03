c3a5b6024ac15b937358224e8b1a59288f21a264
Saya1117 <147129767+Saya1117@users.noreply.github.com>
2024-12-03 17:57:58 +0900

    CreateStartpage

---
+++ flutter_app/lib/pages/startpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Startpage extends StatelessWidget {
+  const Startpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Column(children: [const Text("apple")],),),);
+  }
+}
